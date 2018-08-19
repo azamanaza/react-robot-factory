e22a91cf4afe296c30e325093737d21d642e63d1
Miguel Felipe G. Calo <miguelfelipe.calo@gmail.com>
2018-08-19 15:06:30 +0800

    webpack bundle updates

--- webpack.config.js
+++ webpack.config.js
@@ -1,8 +1,37 @@
+const MiniCssExtractPlugin = require("mini-css-extract-plugin");
+const path = require("path");
+
+const pluginsConfig = [
+    new MiniCssExtractPlugin({
+        // Options similar to the same options in webpackOptions.output
+        // both options are optional
+        filename: "./css/[name].css",
+        chunkFilename: "./css/[id].css"
+    })
+];
+
+const moduleRules = [
+    // All files with a '.ts' or '.tsx' extension will be handled by 'awesome-typescript-loader'.
+    { test: /\.tsx?$/, loader: "awesome-typescript-loader" },
+
+    // All output '.js' files will have any sourcemaps re-processed by 'source-map-loader'.
+    { enforce: "pre", test: /\.js$/, loader: "source-map-loader" },
+
+    { 
+        test: /\.scss$/, 
+        exclude: /node_modules/, 
+        use: [MiniCssExtractPlugin.loader, 'css-loader', 'sass-loader'], 
+    }
+];
+
 module.exports = {
-    entry: "./src/index.tsx",
+    entry: {
+        main: ["./src/js/index.tsx", "./src/css/styles.scss", "./src/css/robot.scss"]
+    },
+
     output: {
-        filename: "bundle.js",
-        path: __dirname + "/dist"
+        filename: "./js/[name].js",
+        path: path.resolve("dist")
     },
 
     // Enable sourcemaps for debugging webpack's output.
@@ -14,15 +43,10 @@ module.exports = {
     },
 
     module: {
-        rules: [
-            // All files with a '.ts' or '.tsx' extension will be handled by 'awesome-typescript-loader'.
-            { test: /\.tsx?$/, loader: "awesome-typescript-loader" },
-
-            // All output '.js' files will have any sourcemaps re-processed by 'source-map-loader'.
-            { enforce: "pre", test: /\.js$/, loader: "source-map-loader" }
-        ]
+        rules: moduleRules
     },
 
+    plugins: pluginsConfig,
     // When importing a module whose path matches one of the following, just
     // assume a corresponding global variable exists and use that instead.
     // This is important because it allows us to avoid bundling all of our
