a749a6001df9a1c691764275246056275e443e4c
Miguel Felipe G. Calo <miguelfelipe.calo@gmail.com>
2018-09-09 22:37:35 +0800

    Added test coverage as viewable page in express server

--- src/js/server/config.ts
+++ src/js/server/config.ts
@@ -2,13 +2,17 @@ const path = require("path");
 
 export interface AppConfig {
     PORT: string | number;
+    rootPath: string;
     distPath: string;
     publicPath: string;
 }
+
+const projectRoot = path.join(__dirname, "../..");
 const config = {
     PORT: 3000,
-    distPath: path.join(__dirname, "../../dist"),
-    publicPath: path.join(__dirname, "../../public"),
+    rootPath: projectRoot,
+    distPath: path.join(projectRoot, "/dist"),
+    publicPath: path.join(projectRoot, "/public"),
 } as AppConfig;
 
 export default config;
